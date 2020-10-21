3264b455082f1a770d666ed57b300416536c6937
Voidilov <voidilov@gmail.com>
2020-10-21 17:58:02 +0300

    no message

--- Package.swift
+++ Package.swift
@@ -25,9 +25,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "VDArch",
-            dependencies: ["VDKit"],
-            dependencies: ["RxSwift"],
-            dependencies: ["RxOperators"]
+            dependencies: ["VDKit", "RxSwift", "RxOperators"]
         ),
         .testTarget(
             name: "VDArchTests",
