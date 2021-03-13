ccb20168a36cfed5b77a4c62a51011bb60e6e4ed
Danil <voidilov@gmail.com>
2021-03-13 16:39:36 +0300

    1.50.0

--- Sources/VDArch/MVVM.swift
+++ Sources/VDArch/MVVM.swift
@@ -66,9 +66,9 @@ extension ViewProtocol {
 		let cancellable = CancellablePublisher()
 		let cancel: Single<Void, Never> = cancellable.merge(with: cancelBinding).share().asSingle()
 		let source = store.cb.prefix(untilOutputFrom: cancel).map(getter).skipEqual()
-		let signal = source.map { viewModel.map(state: $0) }.asSignal()
+		let signal = source.map { viewModel.map(state: $0) }.asState()
 		signal.subscribe(properties)
-		bind(state: StateSignal(signal))
+		bind(state: signal)
 		events.prefix(untilOutputFrom: cancel).flatMap {[weak store] event -> AnyPublisher<Action, Never> in
 			guard let state = store?.state else { return Empty(completeImmediately: false).any() }
 			return viewModel.map(event: event, state: getter(state)).skipFailure().any()
@@ -87,9 +87,9 @@ extension ViewProtocol {
 		} else {
 			source = modelStore.cb.prefix(untilOutputFrom: cancel).combineLatest(viewStore.cb).map(getter).skipEqual().any()
 		}
-		let signal = source.map { viewModel.map(state: $0) }.asSignal()
+		let signal = source.map { viewModel.map(state: $0) }.asState()
 		signal.subscribe(properties)
-		bind(state: StateSignal(signal))
+		bind(state: signal)
 		
 		let cbEvents = events.skipFailure().flatMap {[weak viewStore, weak modelStore] event -> AnyPublisher<Action, Never> in
 			guard let model = modelStore?.state, let view = viewStore?.state else { return Empty<Action, Never>(completeImmediately: false).any() }
--- Sources/VDArch/StateDriver.swift
+++ Sources/VDArch/StateDriver.swift
@@ -19,14 +19,32 @@ public typealias StateDriver<T> = StateSignal<T>
 @dynamicMemberLookup
 public struct StateSignal<Output>: Publisher {
 	public typealias Failure = Never
-	private let signal: Signal<Output>
+	fileprivate let signal: AnyPublisher<Output, Never>
+	private let needMain: Bool
 	
 	public init<P: Publisher>(_ publisher: P) where P.Output == Output {
-		self.signal = publisher.asSignal()
+		self.signal = publisher.skipFailure().any()
+		needMain = true
 	}
 	
-	public init(_ signal: Signal<Output>) {
+	public init(signal: Signal<Output>) {
+		self.signal = signal.any()
+		needMain = false
+	}
+	
+	public init(driver: Driver<Output>) {
+		self.signal = driver.any()
+		needMain = false
+	}
+	
+	public init(signal: StateSignal<Output>) {
+		self.signal = signal.signal
+		needMain = signal.needMain
+	}
+	
+	fileprivate init(any signal: AnyPublisher<Output, Never>, needMain: Bool) {
 		self.signal = signal
+		self.needMain = needMain
 	}
 	
 	public init(just: Output) {
@@ -38,24 +56,27 @@ public struct StateSignal<Output>: Publisher {
 	}
 	
 	public func map<T>(_ selector: @escaping (Output) -> T) -> StateSignal<T> {
-		return StateSignal<T>(signal.map(selector))
+		StateSignal<T>(any: signal.map(selector).any(), needMain: needMain)
 	}
 	
 	public func compactMap<T>(_ selector: @escaping (Output) -> T?) -> StateSignal<T> {
-		StateSignal<T>(signal.compactMap(selector))
+		StateSignal<T>(any: signal.compactMap(selector).any(), needMain: needMain)
 	}
 	
 	public func receive<S>(subscriber: S) where S : Subscriber, Never == S.Failure, Output == S.Input {
-		signal.receive(subscriber: subscriber)
+		if needMain {
+			signal.receive(subscriber: MainQueueSubscriber(subscriber: subscriber))
+		} else {
+			signal.receive(subscriber: subscriber)
+		}
 	}
-	
 }
 
 @available(iOS 13.0, *)
 extension StateSignal where Output: Equatable {
 	
 	public func skipEqual() -> StateSignal {
-		StateSignal(signal.removeDuplicates())
+		StateSignal(any: signal.removeDuplicates().any(), needMain: needMain)
 	}
 }
 
