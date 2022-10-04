4ad6ace08c22f0a33efaa07428ddebc612cfabbe
dankinsoid <30962149+dankinsoid@users.noreply.github.com>
2022-10-04 21:18:59 +0300

    update

--- Sources/VDArch/ReFlow.swift
+++ Sources/VDArch/ReFlow.swift
@@ -8,6 +8,10 @@ public final class FlowCoordinator<Step> {
     public init<F: Flow>(flow: F) where F.Step == Step {
         navigate = flow.navigate
     }
+    
+    public init(_ navigate: @escaping (Step) -> Void) {
+        self.navigate = navigate
+    }
 	
     public func navigate(to step: Step) {
         navigate(step)
@@ -16,6 +20,12 @@ public final class FlowCoordinator<Step> {
     public nonisolated func effect<A>(to step: Step) -> Effect<A, Never> {
         .fireAndForget { [self] in
             await navigate(to: step)
+        }
+    }
+    
+    public func pullback<S>(_ transform: @escaping (S) -> Step) -> FlowCoordinator<S> {
+        FlowCoordinator<S> { [navigate] in
+            navigate(transform($0))
         }
     }
 }
